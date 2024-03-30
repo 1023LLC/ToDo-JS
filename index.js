9919870f765cd70c0be987b6c0274a6d26632825
julius <nyerereoffice@gmail.com>
2024-03-30 08:03:01 +0300

    Implemented the AddItem functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let items = [];
 
 const itemsDiv = document.getElementById('items')
+const input = document.getElementById('itemInput')
 
 
 function renderItems(){
@@ -32,7 +33,16 @@ function loadItems(){}
 
 function saveItems(){}
 
-function addItem(){}
+function addItem(){
+    const value = input.value
+    if (!value){
+        alert("You cannot add an empty item")
+        return
+    }
+    items.push(value)
+    renderItems()
+    input.value = ""
+}
 
 function removeItem(idx){
     items.splice(idx, 1)
